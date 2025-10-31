ea6552f9944d8c30dfed6b87724c6367feaea084
남은솔 <106670648+nameunsol@users.noreply.github.com>
2025-11-01 00:43:07 +0900

    타이틀 완성

--- PianStation/Assets/_ES/ES_Scripts/EffectManager.cs
+++ PianStation/Assets/_ES/ES_Scripts/EffectManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EffectManager : MonoBehaviour
 {
-    public ScreenShake screenShake;
+    public UIScreenShake screenShake;
     public BlinkEffect blinkEffect;
 
     public float minInterval = 2f;
@@ -21,12 +21,10 @@ public class EffectManager : MonoBehaviour
             float waitTime = Random.Range(minInterval, maxInterval);
             yield return new WaitForSeconds(waitTime);
 
-            // ¨ç È­¸é Èçµé¸² ½ÇÇà
-            float shakeTime = Random.Range(0.3f, 1f);
-            float shakePower = Random.Range(0.05f, 0.2f);
+            float shakeTime = Random.Range(0.4f, 1f);
+            float shakePower = Random.Range(5f, 10f);
             screenShake.TriggerShake(shakeTime, shakePower);
 
-            // ¨è È­¸é ±ôºýÀÓ ½ÇÇà
             float blinkTime = Random.Range(0.5f, 1.5f);
             StartCoroutine(blinkEffect.Flash(blinkTime));
         }
